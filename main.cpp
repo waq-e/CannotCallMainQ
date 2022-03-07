d2205f46cb9b6fa370da050d2be1430a0af91a5a
waq-e <whomomlikes@hotmail.com>
2022-03-06 21:29:03 -0600

    Added multiply and divide functions.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 
-int returnFive() {
-	return 5;
-}
+int add(int x, int y) { return x + y; }
+
+int multiply(int x, int y) { return x * y; }
+
+int returnFive() { return 5; }
 
 int main() {
 	std::cout << returnFive << '\n';
 
-	main();  // hmm, learncpp.com wrong, again!!?  &-:
+	// main();  // hmm, learncpp.com wrong, again!!?  &-:
+
+	std::cout << add(4, 5) << '\n';
+
+	// within add() x=3, y=12, so x+y=15
+	std::cout << add(1 + 2, 3 * 4) << '\n';
+
+	int a{ 5 };
+	std::cout << add(a, a) << '\n';
+
+	// evaluates 1 + (2 * 3)
+	std::cout << add(1, multiply(2, 3)) << '\n';
+
+	// evaluates 1 + (2 + 3)
+	std::cout << add(1, add(2, 3)) << '\n';
+
 	return 0;
 }
